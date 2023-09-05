d5c0554c618fe901c19912385dd95eeb9dac4636
AyMeeko <87551537+AyMeeko@users.noreply.github.com>
2023-09-05 09:07:07 -0500

    fix lsp autocomplete

--- nvim-lazy/init.lua
+++ nvim-lazy/init.lua
@@ -483,20 +483,6 @@ local lsp = require("lsp-zero").preset("recommended")
 -- Fix Undefined global 'vim'
 lsp.nvim_workspace()
 
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-  ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-  ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
-})
-
-cmp_mappings["<Tab>"] = nil
-cmp_mappings["<S-Tab>"] = nil
-
-lsp.setup_nvim_cmp({mapping = cmp_mappings})
-
 lsp.set_preferences({
     suggest_lsp_servers = false,
     sign_icons = {
@@ -659,12 +645,17 @@ require("nvim-treesitter.configs").setup {
   },
 }
 
----- SNIPPETS ---
+---- SNIPPETS AND LSP AUTOCOMPLETE ---
+local cmp = require('cmp')
 local luasnip = require("luasnip")
+
 cmp.setup({
-  sources = {
-    {name="luasnip"},
-  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+     { name = 'luasnip' },
+  }, {
+    { name = 'buffer' },
+  }),
   mapping = {
     ["<C-y>"] = cmp.mapping.confirm { select = true },
     ["<C-n>"] = cmp.mapping(function(fallback)
