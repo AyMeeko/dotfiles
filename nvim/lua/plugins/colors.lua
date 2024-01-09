59217a2a4f260b16bef109211641c3d873d62580
AyMeeko <87551537+AyMeeko@users.noreply.github.com>
2024-01-09 14:17:29 -0600

    more colors

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -6,24 +6,51 @@ return {
     config = function()
       require("catppuccin").setup({
         flavour = "macchiato",
+        custom_highlights = function(colors)
+          return {
+            DiagnosticVirtualTextError = { bg = colors.none },
+            DiagnosticVirtualTextWarn = { bg = colors.none },
+            DiagnosticVirtualTextInfo = { bg = colors.none },
+            DiagnosticVirtualTextHint = { bg = colors.none },
+          }
+        end,
         integrations = {
           cmp = true,
+          fidget = true,
           gitsigns = true,
-          nvimtree = true,
-          treesitter = true,
-          notify = false,
-          telescope = {
-            enabled = true,
-             style = "nvchad"
-          },
-          vimwiki = true,
+          headlines = true,
           indent_blankline = {
             enabled = true,
             scope_color = "", -- catppuccin color (eg. `lavender`) Default: text
             colored_indent_levels = false,
           },
-          headlines = true,
-          fidget = true,
+          mason = true,
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+              errors = { "italic" },
+              hints = { "italic" },
+              warnings = { "italic" },
+              information = { "italic" },
+            },
+            underlines = {
+              errors = { "underline" },
+              hints = { "underline" },
+              warnings = { "underline" },
+              information = { "underline" },
+            },
+            inlay_hints = {
+              background = true,
+            },
+          },
+          notify = false,
+          nvimtree = true,
+          telescope = {
+            enabled = true,
+             style = "nvchad"
+          },
+          treesitter = true,
+          vimwiki = true,
         }
       })
       vim.cmd.colorscheme "catppuccin"
