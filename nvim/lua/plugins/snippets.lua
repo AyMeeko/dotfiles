4af1859ace9e43e6e7a0525b55527f3e9f367f00
AyMeeko <87551537+AyMeeko@users.noreply.github.com>
2024-04-08 15:29:50 -0500

    add private snippets

--- nvim/lua/plugins/snippets.lua
+++ nvim/lua/plugins/snippets.lua
@@ -85,6 +85,7 @@ return {
         },
       })
       require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_lua").load({paths = "~/src/personal/private-snippets"})
     end,
   }
 }
