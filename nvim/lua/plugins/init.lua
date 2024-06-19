31e466d1c4888361515d446faaf8dc0033301411
AyMeeko <87551537+AyMeeko@users.noreply.github.com>
2024-06-19 15:35:44 -0500

    todo-comments

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -7,6 +7,16 @@ return {
   },
 
   {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  },
+
+  {
     "j-hui/fidget.nvim",
     config = function()
       require("fidget").setup({})
